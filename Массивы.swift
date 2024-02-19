91c08d8c5febef3cc438af6ad4510989b39e7576
Kris-Suleimanova <112935778+Kris-Suleimanova@users.noreply.github.com>
2024-02-19 12:43:16 +0300

    Add files via upload

---
+++ Массивы.swift
@@ -0,0 +1,34 @@
+var day = [Int]()
+var Mon: [String] = []
+
+day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+Mon = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+
+
+for k in day {
+    print(k)
+}
+
+for k in 0..<Mon.count {
+    print("\(Mon[k]) \(day[k])")
+}
+
+var h = [(Mon: String, day: Int)]()
+for k in 0..<Mon.count {
+    let item = (Mon[k], day[k])
+    h.append(item)
+}
+print(h)
+
+for  k in 0..<Mon.count {
+    print("\(Mon[Mon.count - k - 1]) \(day[Mon.count - k - 1])")
+}
+
+var month = 12
+var days = 1
+var s = days
+
+for  k in 1..<month {
+    s += day[k]
+}
+print(s)
